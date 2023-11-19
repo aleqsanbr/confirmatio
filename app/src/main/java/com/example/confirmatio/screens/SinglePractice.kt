e4cf4ae83a0e37b0987263c2d266539354281793
Виссарионова Анна <anna.ogneva.455@mail.ru>
2023-11-19 14:28:18 +0300

    changed "not finished practice" message

--- app/src/main/java/com/example/confirmatio/screens/SinglePractice.kt
+++ app/src/main/java/com/example/confirmatio/screens/SinglePractice.kt
@@ -1,8 +1,13 @@
 package com.example.confirmatio.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -11,6 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import com.example.confirmatio.practices.PieTechnique
 
 @Composable
@@ -25,11 +33,12 @@ fun SinglePractice(
         {
             Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
             )
             {
-                Text(text = "Screen id ${practiceId}")
+                Text(text = "Данная практика еще не реализована!",
+                    fontSize = 4.em)
                 Button(onClick = navigateUp) {
                     Icon(
                         imageVector = Icons.Default.ArrowBack,
