9bdc60dcc834bc6e27d8928a1124922b702b4340
Руслан Бичурин <bichurin.ruslan.romanovich@yandex.ru>
2023-11-27 22:40:09 +0300

    added customization of the text in the article

--- app/src/main/java/com/example/confirmatio/Templates.kt
+++ app/src/main/java/com/example/confirmatio/Templates.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
@@ -28,7 +29,7 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun Title(title : String, isCentered : Boolean = false) {
+fun Title(title: String, isCentered: Boolean = false) {
     Text(
         text = title,
         fontSize = 30.sp,
@@ -37,12 +38,12 @@ fun Title(title : String, isCentered : Boolean = false) {
         modifier = Modifier
             .padding(15.dp, 30.dp, 15.dp, 15.dp)
             .fillMaxWidth(1f),
-        textAlign = if(isCentered) TextAlign.Center else TextAlign.Left,
+        textAlign = if (isCentered) TextAlign.Center else TextAlign.Left,
     )
 }
 
 @Composable
-fun SubTitle(title : String) {
+fun SubTitle(title: String) {
     Text(
         text = title,
         fontSize = 20.sp,
@@ -85,10 +86,10 @@ fun NavigateUpButton(navigateUp: () -> Unit) {
 
 
 val boldRegex = Regex("(?<!\\*)\\*\\*(?!\\*).*?(?<!\\*)\\*\\*(?!\\*)")
-
+val ItalicRegex = Regex("(?<!\\|)\\|\\|(?!\\|).*?(?<!\\|)\\|\\|(?!\\|)")
 
 @Composable
-fun CustomText(text: String, modifier: Modifier = Modifier, fontSize : TextUnit) {
+fun CustomText(text: String, modifier: Modifier = Modifier, fontSize: TextUnit) {
 
     var results: MatchResult? = boldRegex.find(text)
 
@@ -98,7 +99,7 @@ fun CustomText(text: String, modifier: Modifier = Modifier, fontSize : TextUnit)
 
     var finalText = text
 
-    var color : Color = Color.Black
+    var color: Color = Color.Black
 
     while (results != null) {
         keywords.add(results.value)
@@ -109,8 +110,9 @@ fun CustomText(text: String, modifier: Modifier = Modifier, fontSize : TextUnit)
         val indexOf = finalText.indexOf(keyword)
         val newKeyWord = keyword.removeSurrounding("**")
         val value = newKeyWord.toIntOrNull()
-        if(value != null) {
-            color = if(value < 31) Color.Green else if(value < 45) (if(!isSystemInDarkTheme())Color.DarkGray else Color.Yellow) else Color.Red
+        if (value != null) {
+            color =
+                if (value < 31) Color.Green else if (value < 45) (if (!isSystemInDarkTheme()) Color.DarkGray else Color.Yellow) else Color.Red
         }
         finalText = finalText.replace(keyword, newKeyWord)
         boldIndexes.add(Pair(indexOf, indexOf + newKeyWord.length))
@@ -141,3 +143,82 @@ fun CustomText(text: String, modifier: Modifier = Modifier, fontSize : TextUnit)
         text = annotatedString
     )
 }
+
+@Composable
+fun CustomTextForArticles(text: String, modifier: Modifier = Modifier, fontSize: TextUnit) {
+
+    var resultsBold: MatchResult? = boldRegex.find(text)
+    val boldIndexes = mutableListOf<Pair<Int, Int>>()
+
+    var resultsItalic: MatchResult? = ItalicRegex.find(text)
+    val italicIndexes = mutableListOf<Pair<Int, Int>>()
+
+    val keywords = mutableListOf<Pair<String, Int>>()
+
+    var finalText = text
+
+    var color: Color = if (!isSystemInDarkTheme()) Color.Black else Color.Red
+
+    while (resultsBold != null) {
+        keywords.add(Pair(resultsBold.value, 0))
+        resultsBold = resultsBold.next()
+    }
+
+    while (resultsItalic != null) {
+        keywords.add(Pair(resultsItalic.value, 1))
+        resultsItalic = resultsItalic.next()
+    }
+    var indexOf1 = -1
+    var indexOf2 = -1
+    keywords.forEach { keyword ->
+
+         if (keyword.second == 0)
+             if (indexOf1 == -1) indexOf1 =  finalText.indexOf(keyword.first)
+        else indexOf2 = finalText.indexOf(keyword.first)
+
+
+        val newKeyWord = if (keyword.second == 0) keyword.first.removeSurrounding("**")
+                         else keyword.first.removeSurrounding("||")
+
+        finalText = finalText.replace(keyword.first, newKeyWord)
+        if (keyword.second == 0) boldIndexes.add(Pair(indexOf1, indexOf1 + newKeyWord.length))
+        else italicIndexes.add(Pair(indexOf2, indexOf2 + newKeyWord.length))
+    }
+
+    val annotatedString = buildAnnotatedString {
+        append(finalText)
+
+        italicIndexes.forEach {
+            addStyle(
+                style = SpanStyle(
+                    fontStyle = FontStyle.Italic,
+                    color = color,
+                    fontSize = fontSize
+
+                ),
+                start = it.first,
+                end = it.second
+            )
+        }
+
+        boldIndexes.forEach {
+            addStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                    color = color,
+                    fontSize = fontSize
+
+                ),
+                start = it.first,
+                end = it.second
+            )
+        }
+
+    }
+    Text(
+        modifier = modifier,
+        fontSize = fontSize,
+        text = annotatedString
+    )
+}
+
