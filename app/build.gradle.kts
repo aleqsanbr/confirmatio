eb310633161f293128d71cf9705ef3d7a16f6763
aleqsanbr <abeliko@sfedu.ru>
2024-04-02 17:03:30 +0300

    start implement notifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,25 +64,25 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.3.0")
     implementation("androidx.media3:media3-exoplayer-dash:1.3.0")
     implementation("androidx.media3:media3-ui:1.3.0")
-    implementation("androidx.compose.material:material-icons-extended:1.6.0-beta01")
-    implementation("androidx.compose.animation:animation:1.6.0-beta01")
+    implementation("androidx.compose.material:material-icons-extended:1.6.4")
+    implementation("androidx.compose.animation:animation:1.6.4")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     implementation("androidx.glance:glance-appwidget:1.0.0")
     implementation("androidx.glance:glance:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -95,8 +95,8 @@ dependencies {
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.google.code.gson:gson:2.9.0")
 
-    implementation("androidx.room:room-runtime:2.6.0") // Библиотека "Room"
-    kapt( "androidx.room:room-compiler:2.6.0")// Кодогенератор
-    implementation ("androidx.room:room-ktx:2.6.0") // Дополнительно для Kotlin Coroutines, Kotlin Flows
+    implementation("androidx.room:room-runtime:2.6.1") // Библиотека "Room"
+    kapt( "androidx.room:room-compiler:2.6.1")// Кодогенератор
+    implementation ("androidx.room:room-ktx:2.6.1") // Дополнительно для Kotlin Coroutines, Kotlin Flows
 
 }
--- app/src/main/java/com/example/confirmatio/screens/Settings.kt
+++ app/src/main/java/com/example/confirmatio/screens/Settings.kt
@@ -1,9 +1,21 @@
 package com.example.confirmatio.screens
 
+import android.Manifest
 import android.annotation.SuppressLint
-import android.graphics.Matrix
-import android.graphics.RectF
-import androidx.compose.foundation.Canvas
+import android.app.Activity
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.icu.util.Calendar
+import android.os.Build
+import android.preference.PreferenceManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -25,19 +37,22 @@ import androidx.compose.material.icons.filled.Code
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Palette
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.asComposePath
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -46,8 +61,9 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.PathParser
-import androidx.media3.extractor.text.Subtitle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -60,6 +76,15 @@ import com.example.confirmatio.NotImplemented
 import com.example.confirmatio.R
 import com.example.confirmatio.SubTitle
 import com.example.confirmatio.Title
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.ui.window.Dialog
+import androidx.core.app.ActivityCompat
 
 @Composable
 fun SettingsButton(icon: ImageVector, title: String, subtitle: String, onClick: () -> Unit) {
@@ -184,10 +209,6 @@ fun SettingsContent(navController: NavController) {
                         "Уведомления", "Для регулярного использования"
                     ) { navController.navigate("notifications") }
                     SettingsButton(
-                        Icons.Filled.Lock,
-                        "Конфиденциальность", "Хранение ваших данных"
-                    ) { navController.navigate("privacy") }
-                    SettingsButton(
                         Icons.Filled.Code,
                         "О приложении", "Информация о версии"
                     ) { navController.navigate("about") }
@@ -207,6 +228,7 @@ fun SettingsContent(navController: NavController) {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun Settings() {
     val navController = rememberNavController()
@@ -214,7 +236,6 @@ fun Settings() {
         composable("settings") { SettingsContent(navController) }
         composable("customize") { CustomizeContent(navController) }
         composable("notifications") { NotificationsContent(navController) }
-        composable("privacy") { PrivacyContent(navController) }
         composable("about") { AboutContent(navController) }
     }
 }
@@ -237,7 +258,11 @@ fun LogoInFooter() {
             .padding(start = 0.dp, top = 0.dp, end = 0.dp, bottom = 0.dp)
             .alpha(2f)
     ) {
-        Image(painter = painterResource(id = R.drawable.darklogopng), contentDescription = "", alpha = 0.5F)
+        Image(
+            painter = painterResource(id = R.drawable.darklogopng),
+            contentDescription = "",
+            alpha = 0.5F
+        )
         Text(
             text = "Confirmatio v1.0",
             textAlign = TextAlign.Center,
@@ -265,13 +290,15 @@ fun AboutContent(navController: NavHostController) {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            Text("Confirmatio - это приложение для психологической помощи, которое поможет " +
-                    "вам справиться с тревожностью и стрессом. Приложение предлагает различные " +
-                    "методики и упражнения для расслабления и улучшения вашего психологического " +
-                    "состояния.\n\nОбращаем ваше внимание, что Confirmatio не является " +
-                    "лекарственным средством. При необходимости обращайтесь к специалисту.\n\n" +
-                    "Разработано в рамках проектной деятельности студентами 1-2 курса Института математики, " +
-                    "механики и компьютерных наук ЮФУ в 2024 году.")
+            Text(
+                "Confirmatio - это приложение для психологической помощи, которое поможет " +
+                        "вам справиться с тревожностью и стрессом. Приложение предлагает различные " +
+                        "методики и упражнения для расслабления и улучшения вашего психологического " +
+                        "состояния.\n\nОбращаем ваше внимание, что Confirmatio не является " +
+                        "лекарственным средством. При необходимости обращайтесь к специалисту.\n\n" +
+                        "Разработано в рамках проектной деятельности студентами 1-2 курса Института математики, " +
+                        "механики и компьютерных наук ЮФУ в 2024 году."
+            )
             Spacer(modifier = Modifier.weight(1f))
             Row(
                 modifier = Modifier.align(Alignment.CenterHorizontally),
@@ -284,28 +311,308 @@ fun AboutContent(navController: NavHostController) {
 }
 
 @Composable
-fun PrivacyContent(navController: NavHostController) {
-    Title(title = "Конфиденциальность")
-    NotImplemented(name = "")
+fun TimePicker(
+    hour: Int,
+    onHourSelected: (Int) -> Unit,
+    minute: Int,
+    onMinuteSelected: (Int) -> Unit
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Spacer(modifier = Modifier.width(16.dp))
+        NumberPicker(
+            value = hour,
+            onValueChange = onHourSelected,
+            range = 0..23
+        )
+        Text(text = "ч", modifier = Modifier.padding(start = 8.dp, end = 8.dp))
+        NumberPicker(
+            value = minute,
+            onValueChange = onMinuteSelected,
+            range = 0..59
+        )
+        Text(text = "мин", modifier = Modifier.padding(start = 8.dp))
+    }
 }
 
 @Composable
-fun NotificationsContent(navController: NavHostController) {
-    Title(title = "Уведомления")
-    NotImplemented(name = "")
+fun NumberPicker(
+    value: Int,
+    onValueChange: (Int) -> Unit,
+    range: IntRange
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.width(80.dp)
+    ) {
+        IconButton(onClick = { onValueChange(value + 1) }) {
+            Icon(Icons.Default.ArrowDropUp, contentDescription = "Increase")
+        }
+        Text(text = value.toString())
+        IconButton(onClick = { onValueChange(value - 1) }) {
+            Icon(Icons.Default.ArrowDropDown, contentDescription = "Decrease")
+        }
+    }
 }
+
+@Composable
+fun SaveButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Text("Сохранить время")
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@Composable
+fun NotificationsContent(navController: NavHostController) {
+    val notificationId = 1
+    val context = LocalContext.current
+    val channelId = "channel_id"
+    val notificationBuilder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.logo_icon)
+        .setContentTitle("Напоминание")
+        .setContentText("Вы нажали на кнопку!")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+    var showPermissionWarningDialog by remember { mutableStateOf(false) }
+
+    var selectedHour by remember { mutableStateOf(0) }
+    var selectedMinute by remember { mutableStateOf(0) }
+    var isTimePickerVisible by remember { mutableStateOf(false) }
+    var isNotImplementedWarningVisible by remember { mutableStateOf(false) }
+
+    Column {
+        Title(title = "Уведомления")
+        val requestPermissionLauncher = rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                showPermissionWarningDialog = false
+                scheduleNotification(context, selectedHour, selectedMinute)
+            } else {
+                showPermissionWarningDialog = true
+            }
+        }
+
+        Column(modifier = Modifier.padding(16.dp).fillMaxSize()) {
+            Button(onClick = {
+                if (ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    val notificationManager = NotificationManagerCompat.from(context)
+                    val channel = NotificationChannel(
+                        channelId,
+                        "Напоминания",
+                        NotificationManager.IMPORTANCE_DEFAULT
+                    )
+                    notificationManager.createNotificationChannel(channel)
+                    notificationManager.notify(notificationId, notificationBuilder.build())
+
+                    scheduleNotification(context, selectedHour, selectedMinute)
+
+                } else {
+                    showPermissionWarningDialog = true
+                    ActivityCompat.requestPermissions(
+                        context as ComponentActivity,
+                        arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                        0
+                    )
+                }
+            }) {
+                Text("Показать тестовое уведомление")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                isTimePickerVisible = false
+                isNotImplementedWarningVisible = true
+            }) {
+                Text("Настроить время напоминания")
+            }
+        }
+
+        if (showPermissionWarningDialog) {
+            AlertDialog(
+                onDismissRequest = {
+                    showPermissionWarningDialog = false
+                },
+                title = { Text("Вы не разрешили уведомления") },
+                text = {
+                    Text(
+                        "Пожалуйста, перейдите в настройки и разрешите показ уведомлений" +
+                                " для Confirmatio"
+                    )
+                },
+                confirmButton = {
+                    Button(onClick = {
+                        showPermissionWarningDialog = false
+                    }) {
+                        Text("Хорошо")
+                    }
+                }
+            )
+        }
+
+        if (isNotImplementedWarningVisible) {
+            AlertDialog(
+                onDismissRequest = {
+                    isNotImplementedWarningVisible = false
+                },
+                title = { Text("Уведомления ещё не реализованы") },
+                text = {
+                    Text(
+                        "Напоминания по расписанию пока не работают"
+                    )
+                },
+                confirmButton = {
+                    Button(onClick = {
+                        isNotImplementedWarningVisible = false
+                    }) {
+                        Text("Вернусь позже")
+                    }
+                }
+            )
+        }
+
+        if (isTimePickerVisible) {
+            TimePickerDialog(
+                selectedHour = selectedHour,
+                onHourSelected = { hour -> selectedHour = hour },
+                selectedMinute = selectedMinute,
+                onMinuteSelected = { minute -> selectedMinute = minute },
+                context = context,
+                onDismiss = {
+                    isTimePickerVisible = false
+                    scheduleNotification(context, selectedHour, selectedMinute)
+                },
+                navController = navController
+            )
+        }
+    }
+}
+
+
+@Composable
+fun TimePickerDialog(
+    selectedHour: Int,
+    onHourSelected: (Int) -> Unit,
+    selectedMinute: Int,
+    onMinuteSelected: (Int) -> Unit,
+    onDismiss: () -> Unit,
+    context: Context,
+    navController: NavHostController,
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Surface(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Выберите время напоминания")
+                Spacer(modifier = Modifier.height(16.dp))
+                TimePicker(
+                    hour = selectedHour,
+                    onHourSelected = onHourSelected,
+                    minute = selectedMinute,
+                    onMinuteSelected = onMinuteSelected
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                SaveButton(
+                    onClick = {
+                        scheduleNotification(context, selectedHour, selectedMinute)
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+fun scheduleNotification(context: Context, selectedHour: Int, selectedMinute: Int) {
+    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    val intent = Intent(context, NotificationReceiver::class.java)
+    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+    val calendar = Calendar.getInstance()
+    calendar.set(Calendar.HOUR_OF_DAY, selectedHour)
+    calendar.set(Calendar.MINUTE, selectedMinute)
+    calendar.set(Calendar.SECOND, 0)
+    var notificationTime = calendar.timeInMillis
+
+    // Если выбранное время уже прошло, то устанавливаем уведомление на следующий день
+    if (System.currentTimeMillis() > notificationTime) {
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        notificationTime = calendar.timeInMillis  // Обновляем notificationTime
+    }
+
+    // Устанавливаем повторяющееся срабатывание на выбранное время каждый день
+    alarmManager.setRepeating(
+        AlarmManager.RTC_WAKEUP,
+        calendar.timeInMillis,
+        AlarmManager.INTERVAL_DAY,
+        pendingIntent
+    )
+
+    // Сохраняем выбранное время в SharedPreferences
+    val sharedPreferences = context.getSharedPreferences("my_preferences", Context.MODE_PRIVATE)
+    with(sharedPreferences.edit()) {
+        putLong("notification_time", notificationTime)
+        apply()
+    }
+}
+
+
+
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val channelId = "channel_id"
+        val notificationId = 1
+
+        val notificationBuilder = NotificationCompat.Builder(context!!, channelId)
+            .setSmallIcon(R.drawable.logo_icon)
+            .setContentTitle("Напоминание")
+            .setContentText("Вы нажали на кнопку!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            notificationManager.notify(notificationId, notificationBuilder.build())
+        }
+    }
+}
+
 
 @Composable
 fun CustomizeContent(navController: NavHostController) {
     Column(modifier = Modifier.fillMaxSize()) {
         Title(title = "Вид приложения")
-        SubTitle(title = "Когда-нибудь здесь можно будет настроить цветовую схему и тему приложения. " +
-                "Пока что это просто заглушка. Но скоро всё будет! :) Посмотрите на этого кота: ")
+        SubTitle(
+            title = "Когда-нибудь здесь можно будет настроить цветовую схему и тему приложения. " +
+                    "Пока что это просто заглушка. Но скоро всё будет! :) Посмотрите на этого кота: "
+        )
         Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
             Image(
                 painterResource(id = R.drawable.cat),
                 contentDescription = "Кот",
-                modifier = Modifier.fillMaxSize().padding(16.dp)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
             )
         }
     }
