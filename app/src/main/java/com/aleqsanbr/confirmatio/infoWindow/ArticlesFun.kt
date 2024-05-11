f167e5478ba6e79490aabef00cab7256b424c2db
Ruslan <115139612+Ruslan2806@users.noreply.github.com>
2024-05-11 21:40:17 +0300

    Revert "fixed bug with cards in info window"
    This reverts commit 5fdae9f5e6ecb89660c5ebdf7a432d7db13ea90b.

--- app/src/main/java/com/aleqsanbr/confirmatio/infoWindow/ArticlesFun.kt
+++ app/src/main/java/com/aleqsanbr/confirmatio/infoWindow/ArticlesFun.kt
@@ -19,15 +19,11 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -45,7 +41,6 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aleqsanbr.compose.md_theme_dark_onBackground
@@ -54,49 +49,102 @@ import com.aleqsanbr.compose.md_theme_dark_onBackground
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ArticlesFun(lst: List<articles>, navigateToArticle: (Int) -> Unit) {
-    val pagerState =
-        rememberPagerState(initialPage = 0, initialPageOffsetFraction = 0f) { lst.size }
-
-    val localDensity = LocalDensity.current
-
-    var columnHeightDp by remember { mutableStateOf(0.dp) }
-
-    Column(
-        modifier = Modifier
-            .background(Color.Transparent)
-            .fillMaxHeight(0.5f)
-            .onGloballyPositioned { coordinates ->
-                columnHeightDp = with(localDensity) { coordinates.size.height.toDp() }
-            },
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-
-        Text(
-            text = "Статьи", fontSize = 25.sp, fontWeight = FontWeight(700),
-            color = md_theme_dark_onBackground,
-            textAlign = TextAlign.Center
-        )
-
-        HorizontalPager(state = pagerState) { item ->
-            Box(
-                modifier = Modifier
-                    .height(columnHeightDp - 80.dp)
-                    .width(LocalConfiguration.current.screenWidthDp.dp)
-                    .background(Color.Transparent)
-                    .padding(horizontal = 15.dp)
-                    .clickable { navigateToArticle(item) },
-                contentAlignment = Alignment.Center
-            ) {
-                Image(
-                    painter = painterResource(id = lst[item].imageId),
-                    contentDescription = lst[item].title,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(shape)
-                )
-            }
+    val lazyListStateArticles = rememberLazyListState()
+    val snapBehavior = rememberSnapFlingBehavior(lazyListState = lazyListStateArticles)
+    val visibleIndex by remember {
+        derivedStateOf {
+            lazyListStateArticles.firstVisibleItemIndex
         }
-        DotsIndicator(lst.size, pagerState.currentPage)
+    }
+    val localDensity = LocalDensity.current
+    var columnHeightDp by remember {
+        mutableStateOf(0.dp)
+    }
+    BoxWithConstraints {
+        Column(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .fillMaxHeight(0.5f)
+                .onGloballyPositioned { coordinates ->
+                    columnHeightDp = with(localDensity) { coordinates.size.height.toDp() }
+                }
+        ) {
+            Text(
+                text = "Статьи", fontSize = 30.sp,  fontWeight = FontWeight(700), modifier = Modifier
+                    .padding(horizontal = 30.dp, vertical = 5.dp),
+                color = md_theme_dark_onBackground
+            )
+            LazyRow(
+                modifier = Modifier
+                    .background(Color.Transparent),
+                state = lazyListStateArticles,
+                flingBehavior = snapBehavior,
+                horizontalArrangement = Arrangement.spacedBy(40.dp)
+            ) {
+                itemsIndexed(lst) { index, item ->
+                    Layout(
+                        modifier = Modifier.clickable { navigateToArticle(index) },
+                        content = {
+                            val shape = RoundedCornerShape(20.dp)
+                            Box(
+                                modifier = Modifier
+                                    .height(columnHeightDp - 70.dp)
+                                    .width(LocalConfiguration.current.screenWidthDp.dp + 5.dp)
+                                    .background(Color.Transparent)
+                                    .padding(horizontal = 15.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Image(
+                                    painter = painterResource(id = item.imageId),
+                                    contentDescription = item.title,
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .clip(shape)
+                                )
+
+                                /*
+                                Box(
+                                    modifier = Modifier
+                                        .height(columnHeightDp - 80.dp)
+                                        .width(LocalConfiguration.current.screenWidthDp.dp - 70.dp)
+                                        .background(Color.Transparent, shape = shape)
+                                        .padding(horizontal = 15.dp),
+                                    contentAlignment = Alignment.Center
+                                )
+                                {
+                                    Text(text = item.title,
+                                        textAlign = TextAlign.Center,
+
+                                        lineHeight = 50.sp,
+                                        fontSize = 22.sp,
+                                        fontWeight = FontWeight(700),
+                                        color = md_theme_dark_onBackground,
+                                        modifier = Modifier
+                                            .padding((10.dp)))
+
+                                }
+                                */
+                            }
+                        },
+                        measurePolicy = { measurables, constraints ->
+                            val placeable = measurables.first().measure(constraints)
+                            val maxWidthInPx = this@BoxWithConstraints.maxWidth.roundToPx()
+                            val itemWidth = placeable.width
+                            val startSpace =
+                                if (index == 0) (maxWidthInPx - itemWidth) / 2 else 0
+                            val endSpace =
+                                if (index == lst.lastIndex) (maxWidthInPx - itemWidth) / 2 else 0
+                            val width = startSpace + placeable.width + endSpace
+                            layout(width, placeable.height) {
+                                val x = if (index == 0) startSpace else 0
+                                placeable.place(x, 0)
+                            }
+                        }
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.padding(3.dp))
+            DotsIndicator(lst.size, visibleIndex)
+        }
     }
 }
